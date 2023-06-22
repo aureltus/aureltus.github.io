de81335e5096b9ae1bbee5bfd741c03f4a4fc86f
aureltus <84066760+aureltus@users.noreply.github.com>
2023-06-22 21:13:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let api = `https://api.openweathermap.org/data/2.5/weather?q=bordeaux&units=metric&appid=bb0fe6f6fb5c73b69aca71deaa0c5da2&lang=fr`
+
+function fetchData() {
+    fetch(api)
+      .then((res) => res.json())
+      .then((result) => weatherDetails(result))
+      
+  }
+  
+  function weatherDetails(info) {
+
+      const city = info.name;
+      const country = info.sys.country;
+      const { description, id , icon} = info.weather[0];
+      const { temp, feels_like, humidity , pressure } = info.main;
+      const {speed} = info.wind;
+        console.log("vitesse vent: ",speed)
+      console.log("temperature", temp)
+    console.log("description: ", description ,"id: ",id,"feels like: ", feels_like)
+console.log("pression: ",pressure,"ville: ", city , country, "icon: ",icon)
+let temp5 = document.querySelector(".deg")
+temp5.innerText = Math.floor(temp)
+}
+      fetchData()
+
+      //getJSON('conf.json', function(donnees) {
+        console.log("ok")
+       // console.debug(donnees);
+      //  $('#r').html('<p><b>Nom</b> : ' + donnees.nom + '</p>');
+      //});
+      function ConfJson() {const testConfig = fetch("conf.json")
+      mode:"no-cors"
+      console.log(testConfig)}
+      ConfJson()
