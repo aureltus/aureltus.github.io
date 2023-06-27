f9ae2e89de6feef9dc93ff42fcb320eac1723571
aureltus <84066760+aureltus@users.noreply.github.com>
2023-06-27 16:17:12 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let apiKey = "bb0fe6f6fb5c73b69aca71deaa0c5da2"
 //let city = "paris"
 
 
-let city = fetchVille()
+let city = `fetchVille()`
 console.log(city)
 let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr`;
 fetchData();
