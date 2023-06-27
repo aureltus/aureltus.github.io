1d90e1a5e509d30dfd5423f28fcea82b269c62c4
aureltus <84066760+aureltus@users.noreply.github.com>
2023-06-27 14:40:49 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let apiKey = "bb0fe6f6fb5c73b69aca71deaa0c5da2"
 //let city = "paris"
-let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr`;
+
 
 let city = fetchVille()
 console.log(city)
-
+let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr`;
 fetchData();
 
 setInterval(() => {fetchData()}, 3600000);
