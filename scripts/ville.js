f4103c555f424b6d51c0aade95491f4f7af1202a
aureltus <84066760+aureltus@users.noreply.github.com>
2023-06-27 19:23:51 +0200

    Update ville.js

--- scripts/ville.js
+++ scripts/ville.js
@@ -1,18 +1,18 @@
   // récuperation conf.json 
-  function fetchVille() {
+  function fetchCity() {
     fetch("conf.json")
       .then((res) => res.json())
       .then((result) => villeJson(result))
   }
-      function fetchVille2() {
+      function fetchCity2() {
         fetch("https://aureltus.github.io/conf.json")
           .then((res) => res.json())
-          .then((result) => villeJson(result))
+          .then((result) => cityJson(result))
           
       
   }
 // recuperation de la ville dans le json
-  function villeJson(info) {
+  function cityJson(info) {
     const city = info.ville;
     api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=fr`;
       console.log(city);
@@ -20,6 +20,3 @@
       fetchData()
 }
 
-     //function test (){ fetch("conf.json")
-     //.then(r => r.text())
-     //.then(body => console.log(body))}
