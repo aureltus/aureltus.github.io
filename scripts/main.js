d66b52f9587b041b8e8946c36508708d374777fd
aureltus <84066760+aureltus@users.noreply.github.com>
2023-06-28 23:23:01 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 let apiKey = "bb0fe6f6fb5c73b69aca71deaa0c5da2"
 let api
-fetchCity2()
+fetchCity()
 
 //pour actualiser les donnees toutes les heures (3600000)
-setInterval(() => {fetchCity2()}, 3600000); //60000 pour toutes les minutes
+setInterval(() => {fetchCity()}, 3600000); //60000 pour toutes les minutes
 
 
 
