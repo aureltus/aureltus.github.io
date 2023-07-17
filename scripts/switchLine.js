272299f41c310b7159a3b33f16bed7c9f6a3c77a
aureltus <84066760+aureltus@users.noreply.github.com>
2023-07-17 23:37:39 +0200

    Add files via upload

--- scripts/switchLine.js
+++ scripts/switchLine.js
@@ -1,4 +1,5 @@
 function switchLine(i) {
+    
     const index = i;
     const feels_like = document.querySelector(".line_feels");
     const pressure = document.querySelector(".line_pressure");
@@ -13,6 +14,37 @@ function switchLine(i) {
     const line15h = document.querySelector(".line_15h");
 
     if (index == 1) {
+
+        feels_like.style.transform += `translateY(-25px)`;
+        feels_like.classList.add("anime");
+        pressure.style.transform += `translateY(-25px)`;
+        pressure.classList.add("anime");
+        humidity.style.transform += `translateY(-25px)`;
+        humidity.classList.add("anime");
+        sun.style.transform += `translateY(-25px)`;
+        sun.classList.add("anime");
+        wind.style.transform += `translateY(-25px)`;
+        wind.classList.add("anime");
+
+        line3h.style.transform += `translateY(25px)`;
+        line3h.classList.remove("anime");
+        line6h.style.transform += `translateY(25px)`;
+        line6h.classList.remove("anime");
+        line9h.style.transform += `translateY(25px)`;
+        line9h.classList.remove("anime");
+        line12h.style.transform += `translateY(25px)`;
+        line12h.classList.remove("anime");
+        line15h.style.transform += `translateY(25px)`;
+        line15h.classList.remove("anime");
+
+        setTimeout(() => {
+
+            line3h.style.transform += `translateY(-25px)`;
+            line6h.style.transform += `translateY(-25px)`;
+            line9h.style.transform += `translateY(-25px)`;
+            line12h.style.transform += `translateY(-25px)`;
+            line15h.style.transform += `translateY(-25px)`;
+
         feels_like.style.display = "none";
         pressure.style.display = "none";
         humidity.style.display = "none";
@@ -23,9 +55,38 @@ function switchLine(i) {
         line6h.style.display = "block";
         line9h.style.display = "block";
         line12h.style.display = "block";
-        line15h.style.display = "block";
+        line15h.style.display = "block";}, 500)
     }
     if (index == 2) {
+        feels_like.style.transform += `translateY(25px)`;
+        feels_like.classList.remove("anime");
+        pressure.style.transform += `translateY(25px)`;
+        pressure.classList.remove("anime");
+        humidity.style.transform += `translateY(25px)`;
+        humidity.classList.remove("anime");
+        sun.style.transform += `translateY(25px)`;
+        sun.classList.remove("anime");
+        wind.style.transform += `translateY(25px)`;
+        wind.classList.remove("anime");
+
+        line3h.style.transform += `translateY(-25px)`;
+        line3h.classList.add("anime");
+        line6h.style.transform += `translateY(-25px)`;
+        line6h.classList.add("anime");
+        line9h.style.transform += `translateY(-25px)`;
+        line9h.classList.add("anime");
+        line12h.style.transform += `translateY(-25px)`;
+        line12h.classList.add("anime");
+        line15h.style.transform += `translateY(-25px)`;
+        line15h.classList.add("anime");
+
+        setTimeout(() => {
+
+            line3h.style.transform += `translateY(25px)`;
+            line6h.style.transform += `translateY(25px)`;
+            line9h.style.transform += `translateY(25px)`;
+            line12h.style.transform += `translateY(25px)`;
+            line15h.style.transform += `translateY(25px)`;
 
         line3h.style.display = "none";
         line6h.style.display = "none";
@@ -37,7 +98,7 @@ function switchLine(i) {
         pressure.style.display = "block";
         humidity.style.display = "block";
         sun.style.display = "block";
-        wind.style.display = "flex";
+        wind.style.display = "flex";}, 500)
     }
 
 }
